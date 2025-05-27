1c771f26854e1200d39661e8e45cf64e98f229b4
0x8n <132759719+0x8n@users.noreply.github.com>
2025-05-27 21:52:51 +1000

    terraform complete - virsh vms started

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,97 +1,77 @@
 terraform {
-    required_providers {
-        libvirt = {
-            source = "dmacvicar/libvirt"
-            version = "~> 0.7.0"
-        }
-        null = {
-            source = "hashicorp/null"
-            version = "~> 3.2"
-        }
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "~> 0.7.0"
     }
+  }
 }
 
 provider "libvirt" {
-    uri = "qemu:///system"
+  uri = "qemu:///system"
 }
 
-output "vm_names" {
-    value = [for vm in libvirt_domain.vm:vm.name]
+variable "vm_definitions" {
+  type = list(object({
+    name    = string
+    ram     = number
+    vcpus   = number
+    disk_gb = number
+    ip      = string
+  }))
+  default = [
+    { name = "vm-k8s-master", ram = 4096, vcpus = 2, disk_gb = 20, ip = "192.168.122.10" },
+    { name = "vm-k8s-node-1", ram = 4096, vcpus = 2, disk_gb = 20, ip = "192.168.122.11" },
+    { name = "vm-k8s-node-2", ram = 4096, vcpus = 2, disk_gb = 20, ip = "192.168.122.12" },
+    { name = "vm-wireguard",  ram = 2048, vcpus = 1,  disk_gb = 10, ip = "192.168.122.13" }
+  ]
 }
 
-variable "base_image" {
-    description = "Relative path to base Fedora image"
-    default = "images/Fedora-Cloud-Base-Generic-42-1.1.x86_64.qcow2"
+resource "libvirt_volume" "disk" {
+  for_each = { for vm in var.vm_definitions : vm.name => vm }
+
+  name   = "${each.key}.qcow2"
+  pool   = "default"
+  source = "images/Fedora-Cloud-Base-Generic-42-1.1.x86_64.qcow2"
+  format = "qcow2"
 }
 
-locals {
-    vm_definitions = [
-        { name = "vm-k8s-master", ram = 4096, vcpus = 2, disk = "20G", ip = "192.168.122.10" },
-        { name = "vm-k8s-node-1", ram = 4096, vcpus = 2, disk = "20G", ip = "192.168.122.11" },
-        { name = "vm-k8s-node-2", ram = 4096, vcpus = 2, disk = "20G", ip = "192.168.122.12" },
-        { name = "vm-wireguard", ram = 4096, vcpus = 1, disk = "10G", ip = "192.168.122.13" },
-    ]
-}
+resource "libvirt_cloudinit_disk" "cloudinit" {
+  for_each = { for vm in var.vm_definitions : vm.name => vm }
 
-# autogen meta-data files
-resource "null_resource" "generate-meta-data" {
-    provisioner "local-exec" {
-        command = "${path.module}/cloudinit/generate-meta-data.sh"
-    }
-
-    triggers = {
-        always_run = "${timestamp()}" # forces rerun on every apply
-    }
-}
-
-# depends on meta-data being present ^^
-resource "libvirt_cloudinit_disk" "init" {
-    for_each = { for vm in local.vm_definitions : vm.name => vm }
-
-    name = "${each.key}-cloudinit.iso"
-    user_data = file("${path.module}/cloudinit/user-data")
-
-    meta_data = templatefile("${path.module}/cloudinit/meta-data.tpl", {
-        vm_name = each.key
-    })
-
-    pool = "default"
-
-    lifecycle {
-        replace_triggered_by = [null_resource.generate-meta-data]
-    }
-}
-
-resource "libvirt_volume" "vm_disk" {
-    for_each = { for vm in local.vm_definitions : vm.name => vm }
-
-    name = "${each.key}.qcow2"
-    source = abspath(var.base_image)
-    pool = "default"
-    format = "qcow2"
+  name      = "${each.key}-cloudinit.iso"
+  user_data = file("${path.module}/cloudinit/user-data")
+  meta_data = templatefile("${path.module}/cloudinit/meta-data.tpl", {
+    vm_name = each.key
+  })
+  pool = "default"
 }
 
 resource "libvirt_domain" "vm" {
-    for_each = { for vm in local.vm_definitions : vm.name => vm }
+  for_each = { for vm in var.vm_definitions : vm.name => vm }
 
-    name = each.key
-    memory = each.value.ram
-    vcpu = each.value.vcpus
+  name   = each.key
+  memory = each.value.ram
+  vcpu   = each.value.vcpus
 
-    console {
-        type = "pty"
-        target_port = "0"
-    }
+  disk {
+    volume_id = libvirt_volume.disk[each.key].id
+  }
 
-    disk {
-        volume_id = libvirt_volume.vm_disk[each.key].id
-    } 
+  network_interface {
+    network_name     = "default"
+    wait_for_lease   = true
+    addresses        = [each.value.ip]
+  }
 
-    network_interface {
-        network_name = "default"
-        wait_for_lease = true
-        addresses = [each.value.ip]
-    }
+  cloudinit = libvirt_cloudinit_disk.cloudinit[each.key].id
 
-    cloudinit = libvirt_cloudinit_disk.init[each.key].id
+  console {
+    type        = "pty"
+    target_port = "0"
+  }
+}
+
+output "vm_ips" {
+  value = [for vm in var.vm_definitions : vm.ip]
 }
