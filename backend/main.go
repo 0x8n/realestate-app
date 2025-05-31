837b0cf046955ab6c7a1cef4ae86d99ec3ad5962
0x8n <132759719+0x8n@users.noreply.github.com>
2025-05-31 21:41:43 +1000

    frontend, backend containers build

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,30 @@
 package main
 
-import("fmt"
- "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Hello from Real Estate Backend ApI!")
+type Property struct {
+	ID       int    `json:"id"`
+	Title    string `json:"title"`
+	Location string `json:"location`
+	Price    int    `json:"price"`
 }
 
-func main(){
-	http.HandleFunc("/api/hello", helloHandler)
-	fmt.Println("Starting server on :8080...")
+var listings = []Property{
+	{1, "Modern Appartment", "New York", 2000},
+	{2, "Cozy Cottage", "Vermont", 1200},
+}
+
+func propertiesHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(listings)
+}
+
+func main() {
+	http.HandleFunc("/api/properties", propertiesHandler)
+	fmt.Println("Starting backend on :8080...")
 	http.ListenAndServe(":8080", nil)
 }
